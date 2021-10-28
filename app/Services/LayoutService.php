4987c7e9de15e9fe5fa4db200de07702f8f8f738
HJ <zsanda520@gmail.com>
2021-10-28 09:40:15 +0800

    新增修改service

--- app/Services/LayoutService.php
+++ app/Services/LayoutService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use App\Repositorys\System\MenuRepository;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LayoutService
 {
@@ -13,8 +14,35 @@ class LayoutService
     }
     public function menu(Request $request)
     {
+        $permissions = $request->user()->getAllPermissions()->pluck('name');
         $routes = $this->menuRepository->index($request);
+        $routes = $this->handle($routes, $permissions);
+
 
         return $routes;
+    }
+
+    private function handle($items, $permissions){
+        return $items->map(function($item) use ($permissions){
+            unset($item->controller, $item->order, $item->menu_id, $item->created_at, $item->updated_at);
+            if(Auth::user()->hasRole("SuperAdmin")){
+                $item->meta = [
+                    "show" => true,
+                    "create" => true,
+                    "edit" => true,
+                    "destroy" => true,
+                ];
+            }else{
+                $item->meta = [
+                    "show" => $permissions->search($item->path.".show") !== false,
+                    "create" => $permissions->search($item->path.".create") !== false,
+                    "edit" => $permissions->search($item->path.".edit")  !== false,
+                    "destroy" => $permissions->search($item->path.".destroy")  !== false,
+                ];
+            }
+            $item->children = $this->handle($item->children, $permissions);
+
+            return $item;
+        });
     }
 }
